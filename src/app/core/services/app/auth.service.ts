734fde80a3bc284b90dd1fcc58c81ab47c080182
Juan Espinosa <juancamiloespinosab@gmail.com>
2023-03-01 10:23:36 -0500

    feat: Crear estructura base del proyecto

---
+++ src/app/core/services/app/auth.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor(public jwtHelperService: JwtHelperService) {}
+
+  isAuthenticated() {
+    const accessToken = this.getAccessToken();
+    return this.jwtHelperService.isTokenExpired(accessToken);
+  }
+
+  saveAccessToken(access_token: string) {
+    localStorage.setItem('access_token', access_token);
+  }
+
+  getAccessToken() {
+    return localStorage.getItem('access_token');
+  }
+}
---
+++ src/app/modules/messages/all/all.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AllRoutingModule } from './all-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AllRoutingModule
+  ]
+})
+export class AllModule { }
