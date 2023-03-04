edc974769b08415ecf3fd50638464c7bb77e00b6
Juan Espinosa <juancamiloespinosab@gmail.com>
2023-03-03 20:24:31 -0500

    refactor(button): Crear conjuto de clases para cada tipo de botón, usando la directiva @apply de tailwind

--- src/app/components/atoms/button/button.component.ts
+++ src/app/components/atoms/button/button.component.ts
@@ -10,43 +10,13 @@ export class ButtonComponent implements OnInit {
   @Input() config!: IButtonConfig;
   @Input() action!: () => void;
 
-  buttonClassList: { [key: string]: string[] } = {
-    primary: ['bg-primary', 'text-secondary', 'px-8', 'py-1', 'rounded-md'],
-    secondary: [
-      'bg-secondary',
-      'text-primary',
-      'border',
-      'border-primary',
-      'px-8',
-      'py-1',
-      'rounded-md',
-    ],
-    'white-transparent': [
-      'text-primary',
-      'border',
-      'border-white',
-      'px-8',
-      'py-1',
-      'rounded-3xl',
-    ],
-    white: [
-      'bg-white',
-      'text-secondary',
-      'border',
-      'border-white',
-      'px-8',
-      'py-1',
-      'rounded-3xl',
-    ],
-    flat: ['text-gray-dark'],
-    blur: [
-      'text-gray-dark',
-      'hover:backdrop-blur-sm',
-      'hover:bg-gray/50',
-      'transition-all',
-      'rounded-xl',
-      'p-10',
-    ],
+  buttonClassList: { [key: string]: string } = {
+    primary: 'button-primary',
+    secondary: 'button-secondary',
+    'white-transparent': 'button-white-transparent',
+    white: 'button-white',
+    flat: 'button-text-gray-dark',
+    blur: 'button-button-blur',
   };
 
   constructor() {}
